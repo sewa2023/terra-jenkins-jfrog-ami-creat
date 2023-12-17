c49be79603c7fd6a3c459cc23ae710d15204faa4
sewa2023 <sewasewavi@yahoo.com>
2023-12-16 23:42:14 -0600

    newcode

---
+++ jenkins-setup/aws-ec2.tf
@@ -0,0 +1,100 @@
+# configured aws provider with proper credentials
+provider "aws" {
+  region    = var.aws_region
+  profile   = var.profile
+}
+
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+# create default subnet if one does not exit
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+  tags   = {
+    Name = "utrains default subnet"
+  }
+}
+
+# create security group for the ec2 instance
+resource "aws_security_group" "jenkins_ec2_security_group" {
+  name        = "ec2 jenkins security group"
+  description = "allow access on ports 8080 and 22"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  # allow access on port 8080 for Jenkins Server
+  ingress {
+    description      = "http proxy access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  # allow access on port 22 ssh connection
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "utrains jenkins server security group"
+  }
+}
+
+# launch the ec2 instance and install jenkis
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.aws_instance_type
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.jenkins_ec2_security_group.id]
+  key_name               = aws_key_pair.jenkins_key.key_name
+  user_data            = file("installjenkins.sh")
+
+  # Attach role to Ec2 instance
+  iam_instance_profile = aws_iam_instance_profile.jenkins_instance_profile.name
+  # Set the instance's root volume to 30 GB
+  root_block_device {
+    volume_size = 30
+  }
+
+  # Attach an additional 30 GB EBS volume
+  ebs_block_device {
+    device_name = "/dev/xvdf"
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "Jenkins-server"
+  }
+}
+
+# Code to create Ami for our Jenkins server
+
+/*
+module "ami" {
+  source = "../ami-creation"
+  source_id = aws_instance.ec2_instance.id
+  ami_name = "jenkins_ami"
+}
+
+output "jenkins_ami_id" {
+  value = module.ami.ami_id
+}
+*/
+
+## terraform state list 
+### terraform state rm module.ami.aws_ami_from_instance.ami
---
+++ jenkins-setup/installjenkins.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -x
+
+#### Autor : Utrains Team
+#### Date : 12-29-2022
+### Modified november 2023 by Prof
+
+sudo yum update -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade -y
+
+## install Start And Enable Docker
+sudo yum install docker -y
+sudo service docker start 
+sudo systemctl enable docker.service
+
+sudo chmod 777  /var/run/docker.sock
+
+
+## Install aws cli
+sudo yum install aws-cli -y
+
+## install Git
+sudo yum install git -y
+yum install unzip -y
+
+## Install Java 11:
+#sudo amazon-linux-extras install java-openjdk11 -y
+
+sudo yum install java-11* -y
+## Install Jenkins then Enable the Jenkins service to start at boot :
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+
+## Start Jenkins as a service:
+sudo systemctl start jenkins
+
+## Display Initial Jenkins Password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+
+#------------------------------------------------------------------------------------#
+# This part of the script Dowload and untar maven, using tar xf ...                  #
+#------------------------------------------------------------------------------------#
+
+# Download mavan to /tmp directory then untar it on /opt after create a symbolic link
+wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz -P /tmp
+sudo tar xf /tmp/apache-maven-3.9.5-bin.tar.gz -C /opt
+sudo mv /opt/apache-maven-3.9.5 /opt/maven
+
+#------------------------------------------------------------------------------------#
+# This part of the script installs  sonar-scanner tool                               #
+#------------------------------------------------------------------------------------#
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.0.2856-linux.zip
+unzip sonar-scanner-cli-4.8.0.2856-linux.zip
+
+sudo mv sonar-scanner-4.8.0.2856-linux/ /opt/sonar-scanner
+
+
+# Clean up downloaded files
+rm -rf sonar-scanner-cli-4.8.0.2856-linux.zip
+
+#-----------------------------------------------------------------------------------------------------#
+# This part of the script configures the JAVA, MAVEN And SONAR_SCANNER environment variables          #
+#-----------------------------------------------------------------------------------------------------#
+JAVA_PATH=`find /usr/lib/jvm/java-11* | head -n 3 | grep 64`
+export JAVA_HOME=$JAVA_PATH
+export M2_HOME=/opt/maven
+export M2=$M2_HOME/bin
+export SONAR_RUNNER_HOME=/opt/sonar-scanner
+export PATH=${JAVA_HOME}:${M2_HOME}:${M2}:${SONAR_RUNNER_HOME}:${PATH}
+
+### Configure the path variable 
+cat > /tmp/maven.sh << EOF
+# Confifuration file for java and maven
+export JAVA_HOME=$JAVA_PATH
+export M2_HOME=/opt/maven
+export M2=$M2_HOME/bin
+export SONAR_RUNNER_HOME=${SONAR_RUNNER_HOME}
+export PATH=${JAVA_HOME}:${M2_HOME}:${M2}:${SONAR_RUNNER_HOME}:${PATH}
+EOF
+
+sudo cp /tmp/maven.sh /etc/profile.d/
+sudo chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+
+
+#------------------------------------------------------------------------------------#
+# This part of the script installs  Install Terraform.                               #
+#------------------------------------------------------------------------------------#
+
+# Install jq package for get Terraform version
+sudo yum install jq -y
+
+# Get the latest version of Terraform from the releases page
+TERRAFORM_VERSION=1.6.5
+# The get the Latest version of Terraform, you can execute this command : 
+# curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M .current_version
+
+# Download and install Terraform
+sudo curl -O "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+sudo mv terraform /usr/local/bin/
+
+# Clean up downloaded files
+rm -f "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+
+#------------------------------------------------------------------------------------#
+# This part of the script to install python3 and all necessary moduls.               #
+#------------------------------------------------------------------------------------#
+sudo yum install python3 -y
+pip3 install requests
+pip3 install boto3
+
